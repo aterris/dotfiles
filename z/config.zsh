e15dcf6f0d612882cf67f24b64a8d7f8329a6363
aterris <atterris@gmail.com>
2016-09-09 16:01:01 -0400

    add z

---
+++ z/config.zsh
@@ -0,0 +1,1 @@
+source $ZSHDOT/z/z
--- zsh/config.zsh
+++ zsh/config.zsh
@@ -26,7 +26,8 @@ setopt HIST_REDUCE_BLANKS
 
 # don't expand aliases _before_ completion has finished
 #   like: git comm-[tab]
-setopt complete_aliases
+# Remove because this breaks z auto completion
+#setopt complete_aliases
 
 zle -N newtab
 
